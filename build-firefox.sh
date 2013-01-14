7b103cd17012c911ec63c8d304204aa1b2716758
Joe Ferner <joe@fernsroth.com>
2013-01-14 15:17:03 -0500

    new firefox

--- build-firefox.sh
+++ build-firefox.sh
@@ -9,10 +9,13 @@ mkdir -p ./build/firefox/skin
 cp ./images/* ./build/firefox/skin
 cp -R ./firefox/* ./build/firefox
 mkdir ./build/firefox/data
+cp ./chrome/content-script-home.js ./build/firefox/data
 cp ./unanet.js ./build/firefox/data
 cp ./libs/* ./build/firefox/data
 
 cd ./build/firefox
+rm unanet-firefox-extension.xpi
+rm ../../firefox/unanet-firefox-extension.xpi
 cfx xpi
 cp unanet-firefox-extension.xpi ../../firefox
 cd ../..
