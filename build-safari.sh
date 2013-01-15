5db5ef867d6d49452c40ac69979c3e8b8849501a
jharwig <jason@pinepointsoftware.com>
2013-01-15 11:20:56 -0600

    Update safari build

--- build-safari.sh
+++ build-safari.sh
@@ -5,8 +5,16 @@ echo Building safari extension...
 rm -rf ./build/safari
 
 mkdir -p ./build/safari
-cp -R ./safari/* ./build/safari
+cp -R ./safari/unanet.safariextension ./build/safari
 cp ./unanet.js ./build/safari/unanet.safariextension
 cp ./libs/* ./build/safari/unanet.safariextension
+
+PlistBuddy=/usr/libexec/PlistBuddy
+
+SHORT=`$PlistBuddy -c "Print :CFBundleShortVersionString" ./safari/unanet.safariextension/Info.plist`
+VERSION=`$PlistBuddy -c "Print :CFBundleVersion" ./safari/unanet.safariextension/Info.plist`
+
+$PlistBuddy -c "Set :Extension\ Updates:0:CFBundleVersion $VERSION" ./safari/manifest.plist
+$PlistBuddy -c "Set :Extension\ Updates:0:CFBundleShortVersionString $SHORT" ./safari/manifest.plist
 
 echo Done building safari extension.
--- safari/README.md
+++ safari/README.md
@@ -6,4 +6,14 @@ Checks if your timecard is complete for the day and shows warning after configur
 Install
 -------
 
-Open [Unanet Safari Extension](https://github.com/nearinfinity/unanet-browser-extension/blob/master/safari/unanet.safariextz?raw=true)
+Download [Unanet Safari Extension](https://raw.github.com/wiki/nearinfinity/unanet-browser-extension/downloads/unanet.safariextz)
+
+
+Developers
+----------
+
+1. Clone this repository
+1. `./build-safari.sh`
+1. Open Safari develop menu -> Extension builder
+1. Install a [Safari Certificate](https://developer.apple.com/support/safari/developer-certificates.html) if you haven't already
+1. Add Extension located in `build/safari/unanet.safariextension
