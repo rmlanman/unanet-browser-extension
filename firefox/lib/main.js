9b4d3d6cecdc3d13899c6936d1f88f71643ce22b
Tyler Vernon <tvernon@nearinfinity.com>
2013-05-08 10:57:05 -0300

    Add additional timecard url.

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -4,7 +4,7 @@ var pageMod = require("page-mod");
 var data = require("self").data;
 
 pageMod.PageMod({
-  include: "https://timecards.nearinfinity.com/action/home",
+  include: ["https://timecards.nearinfinity.com/action/home","https://timecards.altamiracorp.com/action/home"],
   contentScriptFile: [data.url("unanet.js"),
                       data.url("async.min.js"),
                       data.url("jquery-1.8.3.min.js"),
